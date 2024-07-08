41e59ad34f95b854072ebc8c8b9e522e60b36f71
LG-BaoPTIT <bao2002pytn@gmail.com>
2024-07-08 15:12:02 +0700

    update

--- src/main/java/com/ite/authservice/constants/JobQueue.java
+++ src/main/java/com/ite/authservice/constants/JobQueue.java
@@ -12,4 +12,6 @@ public class JobQueue {
     public static final String ADD_ROLE_GROUP_QUEUE = "add-role-group";
     public static final String UPDATE_ROLE = "update-roleGroup";
     public static final String DELETE_ROLE = "delete-roleGroup";
+    public static final String APPROVED_ACCOUNT_NOTICE_QUEUE = "approved-account-notice";
+    public static final String RESET_PASSWORD_NOTICE_QUEUE = "reset-password-notice";
 }
